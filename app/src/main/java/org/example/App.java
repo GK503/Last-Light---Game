48b418d5d974dae0886251c2f7bc6ea7849ac1db
Gowtham Kodali <Gowtham.Kodali@frogforce503.org>
2026-01-18 16:38:15 -0500

    Added Player Movements (Didn't add player image yet)

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import org.graalvm.polyglot.*;
+
+import com.almasb.fxgl.texture.ColoredTexture;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.util.ArrayList;
@@ -15,15 +18,18 @@ public class App extends JFrame implements KeyListener {
             {"chunk4", "chunk5", "chunk6"},
             {"chunk7", "chunk8", "chunk9"}
         };*/
+    int tempx = 0;
+    int tempy = 0;
     public static JFrame frame = new JFrame();
     public static Integer currentChunkX = 0;
     public static Integer currentChunkY = 0;
+    static JLabel player;
     public static ArrayList<Integer> ChunkmapX = new ArrayList<>();
     ArrayList<Integer> ChunkmapY = new ArrayList<>();
     public static void main(String[] args) {
         try (Context context = Context.create("python")) { 
             
-            Value string = context.eval("python", ""
+            Value string = context.eval("python", "" 
                 + "name = \"name\"\n"
                 + "print(f'Hello, {name}!')\n"
             );
@@ -36,22 +42,26 @@ public class App extends JFrame implements KeyListener {
             Value result = context.eval("python", "10 + 5");
             System.out.println("Result from Python: " + result.asInt());
         }
-    }
-    /*
+
         // Start the Swing application by creating the
         //  frame instance
-        frame.setTitle("My Swing Application");
+        frame.setTitle("Last Light");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
         // Make sure the frame is focusable and will receive key events
         frame.setFocusable(true);
         frame.addKeyListener(KeyListener.class.cast(new App()));
         frame.setVisible(true);
-        player("defaultPlayer", Color.RED);
-        player("defaultPlayer", Color.black);
-    */
+        player = Object("defaultPlayer", Color.RED);
+    }
 
-    public static void player(String playername, Color color) {
+    public static void MoveObject(int movex, int movey){
+        System.out.println("MoveObject method called");
+        player.setLocation(movex, movey);
+        System.out.println("Player moved to: (" + movex + ", " + movey + ")");
+    }
+
+    public static JLabel Object(String playername, Color color) {
         System.out.println("Player method called");
         System.out.println("Player name: " + playername);
         JLabel label = new JLabel(playername);
@@ -59,11 +69,13 @@ public class App extends JFrame implements KeyListener {
         label.setBounds(0,0,100,100);
         label.setBackground(color);
         frame.add(label);
+        return label;
     }
 
     //Store different maps/chunks of the map
-    private void storeMaps() {
-        // Implementation for storing maps
+    private void storeMaps(int chunkx, int chunky) {
+        ChunkmapX.add(chunkx);
+        ChunkmapY.add(chunky);
     }
 
     @Override
@@ -72,6 +84,23 @@ public class App extends JFrame implements KeyListener {
         switch (c) {
             case 'a':
                 System.out.println("Key typed: a");
+                tempx -= 10;
+                MoveObject(tempx, tempy);
+                break;
+            case 'd':
+                System.out.println("Key typed: d");
+                tempx += 10;
+                MoveObject(tempx, tempy);
+                break;
+            case 'w':
+                System.out.println("Key typed: w");
+                tempy -= 10;
+                MoveObject(tempx, tempy);
+                break;
+            case 's':
+                System.out.println("Key typed: s");
+                tempy += 10;
+                MoveObject(tempx, tempy);
                 break;
             default:
                 // ignore
